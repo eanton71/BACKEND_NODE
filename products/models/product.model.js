ab5aee635acdb99ed5cc36bfac894b925d393fa9
eanton71 <eanton71@gmail.com>
2022-12-13 20:22:19 +0100

    mover carpetas

---
+++ products/models/product.model.js
@@ -0,0 +1,43 @@
+'use strict'
+const mongoose = require('../../common/servíces/mongoose.service').mongoose;
+const productsSchema = new mongoose.Schema({
+    name: {
+        type: mongoose.Schema.Types.String
+
+    },
+
+    price: {
+        type: mongoose.Schema.Types.Number
+
+    },
+
+    description: {
+
+        type: mongoose.schema.Types.String
+    }
+}, { versionKey: false })
+
+productsSchema.set('toJSON', { virtuals: false });
+const Products = mongoose.model('products', productsSchema, 'products');
+
+exports.getProducts = () => {
+    return new Promise((resolve, reject) => {
+        Products.fínd({}).exec((error, result) => {
+            if (error) {
+                reject(error.message);
+                throw error.message;
+
+            }
+
+            if (result) {
+                resolve(result);
+
+            }
+        })
+    }).catch(error => {
+
+        throw error.message;
+    })
+}
+
+
