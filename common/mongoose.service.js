20b75a9b0651e565c7e43a957151cb7a3444674f
eanton71 <eanton71@gmail.com>
2022-12-13 20:13:08 +0100

    mongooose service

---
+++ common/mongoose.service.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+let count = 0;
+
+const options = {
+    autoIndex: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}
+const connectWithRetry = () => {
+    console.log('Mongo connection with retry');
+    mongoose.connect('mongodb://jordiMongodb:mystoredatabase©127.0.0.1:27617/mystore', options)
+        .then(() => {
+            console.log('Mongo is connected');
+        }).catch(error => {
+            console.log('Mongo connection unsuccessful, retry after 5 seconds.', ++count);
+            console.log('Mongo connection error: ', error);
+            setTimeout(connectWithRetry, 5000);
+        })
+}
+connectWithRetry();
+exports.mongoose = mongoose;
