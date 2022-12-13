e7a31a4daf527f2270608b0a0e927336345d0fda
eanton71 <eanton71@gmail.com>
2022-12-13 19:46:54 +0100

    products.routes

---
+++ routes/products.routes.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const ProductController = require('../controllers/product.controller');
+
+exports.productRoutes = function (app) {
+    app.get('/api/get_products', ProductController.getAllProducts);
+    app.post('/api/add_product', ProductController.addProduct);
+    app.delete('/api/delete_product/:id', ProductController.deleteProduct);
+
+}
